e87eef138f9ead85fddb35a0da71f412360fec7e
Tony Li <dawner125@gmail.com>
2023-11-27 23:37:06 -0800

    Created our first iteration of the Imitation learning model to drive

---
+++ src/imitation_learning/car_controller.py
@@ -0,0 +1,174 @@
+#! /usr/bin/env python3
+
+# Ros imports
+import rospy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist, Vector3
+from std_msgs.msg import String
+
+# To control model response rate
+import time
+
+# General Imports
+import tensorflow as tf
+import numpy as np
+
+# Image processing
+from training.training_image_processing import process_img
+from cv_bridge import CvBridge, CvBridgeError
+
+class car_controller():
+
+    def __init__(self):
+
+        # Subscribe camera topic
+        self.sub_cam = rospy.Subscriber('/R1/pi_camera/image_raw', Image, self.camera_callback)
+
+        # Publish to 
+        self.pub_twist = rospy.Publisher('/R1/cmd_vel', Twist, queue_size=5)
+
+        # Publishers for debugging model predicted commands
+        self.pub_twist_debug = rospy.Publisher('/twist_debug', String, queue_size=5)
+        self.pub_img_debug = rospy.Publisher('/img_debug', Image, queue_size=5)
+
+        # Dictionary that maps Model outputs to real Twist messages
+        self.moves_dictionary = None
+
+        # CNN to drive car
+        self.model = None
+
+        # TODO change from real time to sim time 
+
+        # Timer to prevent overloading car inputs
+        self.cur_time = None
+        self.last_cmd_time = 0
+        self.input_period = 100 # 100 millisecs
+
+    # Takes in a camera image and prepares it to be sent to CNN
+    def camera_callback(self, car_view_img):
+
+        # Update time
+        self.update_cur_time()
+
+        # Only input commands once ever 100 ms
+        if self.cur_time - self.last_cmd_time > self.input_period:
+
+            # Convert Image datatype to numpy array
+            bridge = CvBridge()
+            img_cv2 = bridge.imgmsg_to_cv2(car_view_img, "bgr8")
+
+            # Process image so it is ready to be thrown into the CNN
+            model_ready_img = process_img(img_cv2)
+
+            # TODO Debugging the input to model
+            # self.pub_img_debug.publish(model_ready_img)
+
+            # We need to reshape before passing into CNN            
+            model_ready_img = model_ready_img.reshape((1, 144, 256, 1))
+
+            # Drive! (But only if the model is ready)
+            if self.model != None:
+                self.drive(model_ready_img)
+
+            # Change last command time to cur_time
+            self.last_cmd_time = self.cur_time
+
+    # Update time
+    def update_cur_time(self):
+        self.cur_time = int(time.time() * 1000)
+
+
+    # Given the car's perspective through the camera, navigate the robot
+    def drive(self, model_input):
+        
+        # print(model_input.shape)
+
+        # Get output from CNN, will be a 1D np vector
+        model_pred = self.model.predict(model_input)
+        
+        # Find predicted move by taking max value in output vector
+        pred_idx = np.argmax(model_pred)
+
+        # Go to lookup table to convert from index to Twist Msg
+        twist_command = self.moves_dictionary[pred_idx]
+
+        # Publish to robot
+        self.pub_twist.publish(twist_command)
+
+        # Debugging the output of model 
+        self.pub_twist_debug.publish("________________")
+        self.pub_twist_debug.publish("Predicted Vector: ")
+        self.pub_twist_debug.publish(str(model_pred))
+        self.pub_twist_debug.publish("Equivalent Twist CMD")
+        self.pub_twist_debug.publish(str(twist_command))
+        self.pub_twist_debug.publish("________________")
+
+
+    # Called at start to populate class variables
+    def setup_controller(self, model_path):
+
+        # Load the desired version of the CNN
+        self.model = tf.keras.models.load_model(model_path)
+
+        while self.model == None:
+            print("Waiting for model to load . . .")
+            continue
+        
+        # Setup the dictionary that maps CNN outputs to real Twist Msgs
+        self.moves_dictionary = self.model_output_to_Twist()
+
+
+
+    # Creates a dictionary that maps the output of the CNN to Twist Msgs
+    def model_output_to_Twist(self):
+        
+        # All possibe moves defined as Twist messages
+        curve_left = Twist(linear=Vector3(x=0.5), angular=Vector3(z=1.0))
+        forward = Twist(linear=Vector3(x=0.5), angular=Vector3(z=0.0))
+        curve_right = Twist(linear=Vector3(x=0.5), angular=Vector3(z=-1.0))
+        spin_ccw = Twist(linear=Vector3(x=0.0), angular=Vector3(z=1.0))
+        stop = Twist(linear=Vector3(x=0.0), angular=Vector3(z=0.0))
+        spin_cw = Twist(linear=Vector3(x=0.0), angular=Vector3(z=-1.0))
+        curve_bleft = Twist(linear=Vector3(x=-0.5), angular=Vector3(z=-1.0))
+        backward = Twist(linear=Vector3(x=-0.5), angular=Vector3(z=0.0))
+        curve_bright = Twist(linear=Vector3(x=-0.5), angular=Vector3(z=1.0))
+
+        # Map output vector indices to Twist messages
+        moves_dict = {
+            0: curve_left,
+            1: forward,
+            2: curve_right,
+            3: spin_ccw,
+            4: stop,
+            5: spin_cw,
+            6: curve_bleft,
+            7: backward,
+            8: curve_bright,
+        }
+
+        return moves_dict
+       
+
+
+def main():
+
+    rospy.init_node('car_controller', anonymous=True)
+    
+    # Create an instance of our controller
+    controller = car_controller()
+
+    # Startup sequence of controller
+    model_path = "/home/fizzer/ros_ws/src/controller_repo/src/imitation_learning/cnn_models/imit_model_1.2.h5"
+    # controller.model = tf.keras.models.load_model(model_path)
+    controller.setup_controller(model_path)
+
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+if __name__ == '__main__':
+    main()
+
+    
---
+++ src/imitation_learning/training/twist_serialization.py
@@ -0,0 +1,50 @@
+from os import path
+import json
+from imitation_learning.training.file_indexer import get_file_idx
+
+json_file_name = 'twist_json_file.json'
+
+
+# Deprecated
+def export_twist(cur_twist):
+
+    # File naming index
+    file_index = get_file_idx()
+
+    # Convert our Twist message to a serializable dictionary object
+    twist_d = twist_2_dict(cur_twist)
+
+    # Dict to populate JSON file with
+    entries = {}
+
+    # If the file already exists we will add to it
+    if path.exists(json_file_name):
+
+        # load existing entries
+        with open(json_file_name, 'r') as export_file:
+            entries = json.load(export_file)
+    
+    # Update by appending new entry
+    entries[str(file_index)] = twist_d
+
+    with open(json_file_name, 'w') as export_file:
+        json.dump(entries, export_file, indent=4)
+
+
+# Converts a Twist message to a dictionary
+def twist_2_dict(twist):
+
+    t_dict = {
+        "linear": {
+            "x": twist.linear.x,
+            "y": twist.linear.y,
+            "z": twist.linear.z
+        },
+        "angular": {
+            "x": twist.angular.x,
+            "y": twist.angular.y,
+            "z": twist.angular.z
+        }
+    }
+
+    return t_dict
